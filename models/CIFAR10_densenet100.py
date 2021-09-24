5f948cacd9e875acef4ef6fcd3ad9879da861a89
Berkay Barlas <fpberkay@gmail.com>
2021-09-24 14:13:16 -0400

    try cifar100

--- models/CIFAR10_densenet100.py
+++ models/CIFAR10_densenet100.py
@@ -1,12 +1,26 @@
 from keras.layers import Input, Dense, Activation, Conv2D, AveragePooling2D
 from keras.layers.normalization import BatchNormalization
-from keras.layers.pooling import GlobalAveragePooling2D
+from keras.layers.pooling import AveragePooling2D, MaxPooling2D,GlobalAveragePooling2D
 from keras.layers.merge import concatenate
 from keras.regularizers import l2
 from keras.models import Model
 import keras.backend as K
 import os
 
+from keras import backend as K
+from keras.engine import Layer
+from keras.utils.generic_utils import get_custom_objects
+
+import keras.backend.tensorflow_backend as K_BACKEND
+
+from keras.layers.core import Dense, Dropout, Activation, Reshape
+from keras.layers.convolutional import Conv2D, Conv2DTranspose, UpSampling2D
+from keras.layers.pooling import AveragePooling2D, MaxPooling2D
+from keras.layers.pooling import GlobalAveragePooling2D
+from keras.layers import Input
+from keras.utils.layer_utils import convert_all_kernels_in_model, convert_dense_weights_data_format
+from keras.applications.imagenet_utils import decode_predictions
+import keras.backend as K
 
 def CIFAR10_densenet100(use_softmax=True, rel_path='./'):
     nb_classes = 10
@@ -20,41 +34,123 @@ def CIFAR10_densenet100(use_softmax=True, rel_path='./'):
     inputs = Input(shape=(32, 32, 3))
 
     x = __create_dense_net(nb_classes, inputs, use_softmax, depth, nb_dense_block,
-                           growth_rate, nb_filter, -1, False, 0.0, 1E-4, activation)
+                           growth_rate, nb_filter, -1, False, 0.0, None, 1E-4, False, activation)
 
     # Create model.
     model = Model(inputs, x, name='CIFAR10_densenet100')
-    model.load_weights(os.path.join('%smodels/weights' % rel_path, "CIFAR10_densenet.keras_weights.h5"))
+    model.load_weights(os.path.join('%smodels/weights' % rel_path, "DenseNet-BC-121-32-no-top.h5"))
     model.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=['acc'])
     return model
-
-
-def __transition_block(ip, nb_filter, compression=1.0, weight_decay=1E-4):
+    
+def __conv_block(ip, nb_filter, bottleneck=False, dropout_rate=None, weight_decay=1e-4):
+    ''' Apply BatchNorm, Relu, 3x3 Conv2D, optional bottleneck block and dropout
+    Args:
+        ip: Input keras tensor
+        nb_filter: number of filters
+        bottleneck: add bottleneck block
+        dropout_rate: dropout rate
+        weight_decay: weight decay factor
+    Returns: keras tensor with batch_norm, relu and convolution2d added (optional bottleneck)
+    '''
     concat_axis = 1 if K.image_data_format() == 'channels_first' else -1
-    x = BatchNormalization(axis=concat_axis, gamma_regularizer=l2(weight_decay), beta_regularizer=l2(weight_decay))(ip)
-    x = Activation('relu')(x)
-    x = Conv2D(int(nb_filter * compression), (1, 1), kernel_initializer='he_uniform', padding='same', use_bias=False,
-               kernel_regularizer=l2(weight_decay))(x)
-    x = AveragePooling2D((2, 2), strides=(2, 2))(x)
-    return x
 
-
-def __conv_block(ip, nb_filter, bottleneck=False, weight_decay=1E-4):
-    concat_axis = 1 if K.image_data_format() == 'channels_first' else -1
-    x = BatchNormalization(axis=concat_axis, gamma_regularizer=l2(weight_decay), beta_regularizer=l2(weight_decay))(ip)
+    x = BatchNormalization(axis=concat_axis, epsilon=1.1e-5)(ip)
     x = Activation('relu')(x)
+
     if bottleneck:
         inter_channel = nb_filter * 4  # Obtained from https://github.com/liuzhuang13/DenseNet/blob/master/densenet.lua
-        x = Conv2D(inter_channel, (1, 1), kernel_initializer='he_uniform', padding='same', use_bias=False,
+
+        x = Conv2D(inter_channel, (1, 1), kernel_initializer='he_normal', padding='same', use_bias=False,
                    kernel_regularizer=l2(weight_decay))(x)
-        x = BatchNormalization(axis=concat_axis, gamma_regularizer=l2(weight_decay),
-                               beta_regularizer=l2(weight_decay))(x)
+        x = BatchNormalization(axis=concat_axis, epsilon=1.1e-5)(x)
         x = Activation('relu')(x)
-    x = Conv2D(nb_filter, (3, 3), kernel_initializer='he_uniform', padding='same', use_bias=False,
-               kernel_regularizer=l2(weight_decay))(x)
+
+    x = Conv2D(nb_filter, (3, 3), kernel_initializer='he_normal', padding='same', use_bias=False)(x)
+    if dropout_rate:
+        x = Dropout(dropout_rate)(x)
+
     return x
 
 
+def __dense_block(x, nb_layers, nb_filter, growth_rate, bottleneck=False, dropout_rate=None, weight_decay=1e-4,
+                  grow_nb_filters=True, return_concat_list=False):
+    ''' Build a dense_block where the output of each conv_block is fed to subsequent ones
+    Args:
+        x: keras tensor
+        nb_layers: the number of layers of conv_block to append to the model.
+        nb_filter: number of filters
+        growth_rate: growth rate
+        bottleneck: bottleneck block
+        dropout_rate: dropout rate
+        weight_decay: weight decay factor
+        grow_nb_filters: flag to decide to allow number of filters to grow
+        return_concat_list: return the list of feature maps along with the actual output
+    Returns: keras tensor with nb_layers of conv_block appended
+    '''
+    concat_axis = 1 if K.image_data_format() == 'channels_first' else -1
+
+    x_list = [x]
+
+    for i in range(nb_layers):
+        cb = __conv_block(x, growth_rate, bottleneck, dropout_rate, weight_decay)
+        x_list.append(cb)
+
+        x = concatenate([x, cb], axis=concat_axis)
+
+        if grow_nb_filters:
+            nb_filter += growth_rate
+
+    if return_concat_list:
+        return x, nb_filter, x_list
+    else:
+        return x, nb_filter
+
+
+def __transition_block(ip, nb_filter, compression=1.0, weight_decay=1e-4):
+    ''' Apply BatchNorm, Relu 1x1, Conv2D, optional compression, dropout and Maxpooling2D
+    Args:
+        ip: keras tensor
+        nb_filter: number of filters
+        compression: calculated as 1 - reduction. Reduces the number of feature maps
+                    in the transition block.
+        dropout_rate: dropout rate
+        weight_decay: weight decay factor
+    Returns: keras tensor, after applying batch_norm, relu-conv, dropout, maxpool
+    '''
+    concat_axis = 1 if K.image_data_format() == 'channels_first' else -1
+
+    x = BatchNormalization(axis=concat_axis, epsilon=1.1e-5)(ip)
+    x = Activation('relu')(x)
+    x = Conv2D(int(nb_filter * compression), (1, 1), kernel_initializer='he_normal', padding='same', use_bias=False,
+               kernel_regularizer=l2(weight_decay))(x)
+    x = AveragePooling2D((2, 2), strides=(2, 2))(x)
+
+    return x
+
+
+def __transition_up_block(ip, nb_filters, type='deconv', weight_decay=1E-4):
+    ''' SubpixelConvolutional Upscaling (factor = 2)
+    Args:
+        ip: keras tensor
+        nb_filters: number of layers
+        type: can be 'upsampling', 'subpixel', 'deconv'. Determines type of upsampling performed
+        weight_decay: weight decay factor
+    Returns: keras tensor, after applying upsampling operation.
+    '''
+
+    if type == 'upsampling':
+        x = UpSampling2D()(ip)
+    elif type == 'subpixel':
+        x = Conv2D(nb_filters, (3, 3), activation='relu', padding='same', kernel_regularizer=l2(weight_decay),
+                   use_bias=False, kernel_initializer='he_normal')(ip)
+        x = SubPixelUpscaling(scale_factor=2)(x)
+        x = Conv2D(nb_filters, (3, 3), activation='relu', padding='same', kernel_regularizer=l2(weight_decay),
+                   use_bias=False, kernel_initializer='he_normal')(x)
+    else:
+        x = Conv2DTranspose(nb_filters, (3, 3), activation='relu', padding='same', strides=(2, 2),
+                            kernel_initializer='he_normal', kernel_regularizer=l2(weight_decay))(ip)
+
+    return x
 def __dense_block(x, nb_layers, nb_filter, growth_rate, bottleneck=False, weight_decay=1E-4,
                   grow_nb_filters=True, return_concat_list=False):
     concat_axis = 1 if K.image_data_format() == 'channels_first' else -1
@@ -71,34 +167,62 @@ def __dense_block(x, nb_layers, nb_filter, growth_rate, bottleneck=False, weight
         return x, nb_filter
 
 
-def __create_dense_net(nb_classes, img_input, use_softmax, depth=40, nb_dense_block=3, growth_rate=12, nb_filter=-1,
-                       nb_layers_per_block=-1, bottleneck=False, reduction=0.0, weight_decay=1E-4,
-                       activation='softmax'):
+def __create_dense_net(nb_classes, img_input, include_top, depth=40, nb_dense_block=3, growth_rate=12, nb_filter=-1,
+                       nb_layers_per_block=-1, bottleneck=False, reduction=0.0, dropout_rate=None, weight_decay=1e-4,
+                       subsample_initial_block=False, activation='softmax'):
+    ''' Build the DenseNet model
+    Args:
+        nb_classes: number of classes
+        img_input: tuple of shape (channels, rows, columns) or (rows, columns, channels)
+        include_top: flag to include the final Dense layer
+        depth: number or layers
+        nb_dense_block: number of dense blocks to add to end (generally = 3)
+        growth_rate: number of filters to add per dense block
+        nb_filter: initial number of filters. Default -1 indicates initial number of filters is 2 * growth_rate
+        nb_layers_per_block: number of layers in each dense block.
+                Can be a -1, positive integer or a list.
+                If -1, calculates nb_layer_per_block from the depth of the network.
+                If positive integer, a set number of layers per dense block.
+                If list, nb_layer is used as provided. Note that list size must
+                be (nb_dense_block + 1)
+        bottleneck: add bottleneck blocks
+        reduction: reduction factor of transition blocks. Note : reduction value is inverted to compute compression
+        dropout_rate: dropout rate
+        weight_decay: weight decay rate
+        subsample_initial_block: Set to True to subsample the initial convolution and
+                add a MaxPool2D before the dense blocks are added.
+        subsample_initial:
+        activation: Type of activation at the top layer. Can be one of 'softmax' or 'sigmoid'.
+                Note that if sigmoid is used, classes must be 1.
+    Returns: keras tensor with nb_layers of conv_block appended
+    '''
+
     concat_axis = 1 if K.image_data_format() == 'channels_first' else -1
 
-    assert (depth - 4) % 3 == 0, 'Depth must be 3 N + 4'
     if reduction != 0.0:
-        assert 0.0 < reduction <= 1.0, 'reduction value must lie between 0.0 and 1.0'
+        assert reduction <= 1.0 and reduction > 0.0, 'reduction value must lie between 0.0 and 1.0'
 
     # layers in each dense block
     if type(nb_layers_per_block) is list or type(nb_layers_per_block) is tuple:
         nb_layers = list(nb_layers_per_block)  # Convert tuple to list
 
-        assert len(nb_layers) == (nb_dense_block + 1), 'If list, nb_layer is used as provided. ' \
-                                                       'Note that list size must be (nb_dense_block + 1)'
+        assert len(nb_layers) == (nb_dense_block), 'If list, nb_layer is used as provided. ' \
+                                                   'Note that list size must be (nb_dense_block)'
         final_nb_layer = nb_layers[-1]
         nb_layers = nb_layers[:-1]
     else:
         if nb_layers_per_block == -1:
+            assert (depth - 4) % 3 == 0, 'Depth must be 3 N + 4 if nb_layers_per_block == -1'
             count = int((depth - 4) / 3)
+
+            if bottleneck:
+                count = count // 2
+
             nb_layers = [count for _ in range(nb_dense_block)]
             final_nb_layer = count
         else:
             final_nb_layer = nb_layers_per_block
             nb_layers = [nb_layers_per_block] * nb_dense_block
-
-    if bottleneck:
-        nb_layers = [int(layer // 2) for layer in nb_layers]
 
     # compute initial nb_filter if -1, else accept users initial nb_filter
     if nb_filter <= 0:
@@ -108,8 +232,20 @@ def __create_dense_net(nb_classes, img_input, use_softmax, depth=40, nb_dense_bl
     compression = 1.0 - reduction
 
     # Initial convolution
-    x = Conv2D(nb_filter, (3, 3), kernel_initializer='he_uniform', padding='same', name='initial_conv2D',
-               use_bias=False, kernel_regularizer=l2(weight_decay))(img_input)
+    if subsample_initial_block:
+        initial_kernel = (7, 7)
+        initial_strides = (2, 2)
+    else:
+        initial_kernel = (3, 3)
+        initial_strides = (1, 1)
+
+    x = Conv2D(nb_filter, initial_kernel, kernel_initializer='he_normal', padding='same',
+               strides=initial_strides, use_bias=False, kernel_regularizer=l2(weight_decay))(img_input)
+
+    if subsample_initial_block:
+        x = BatchNormalization(axis=concat_axis, epsilon=1.1e-5)(x)
+        x = Activation('relu')(x)
+        x = MaxPooling2D((3, 3), strides=(2, 2), padding='same')(x)
 
     # Add dense blocks
     for block_idx in range(nb_dense_block - 1):
@@ -123,12 +259,88 @@ def __create_dense_net(nb_classes, img_input, use_softmax, depth=40, nb_dense_bl
     x, nb_filter = __dense_block(x, final_nb_layer, nb_filter, growth_rate, bottleneck=bottleneck,
                                  weight_decay=weight_decay)
 
-    x = BatchNormalization(axis=concat_axis, gamma_regularizer=l2(weight_decay),
-                           beta_regularizer=l2(weight_decay))(x)
+    x = BatchNormalization(axis=concat_axis, epsilon=1.1e-5)(x)
     x = Activation('relu')(x)
     x = GlobalAveragePooling2D()(x)
 
-    x = Dense(nb_classes, kernel_regularizer=l2(weight_decay), bias_regularizer=l2(weight_decay))(x)
-    if use_softmax:
-        x = Activation(activation)(x)
+    if include_top:
+        x = Dense(nb_classes, activation=activation)(x)
+
     return x
+
+
+def normalize_data_format(value):
+    if value is None:
+        value = K.image_data_format()
+    data_format = value.lower()
+    if data_format not in {'channels_first', 'channels_last'}:
+        raise ValueError('The `data_format` argument must be one of '
+                         '"channels_first", "channels_last". Received: ' +
+                         str(value))
+    return data_format
+    
+class SubPixelUpscaling(Layer):
+    """ Sub-pixel convolutional upscaling layer based on the paper "Real-Time Single Image
+    and Video Super-Resolution Using an Efficient Sub-Pixel Convolutional Neural Network"
+    (https://arxiv.org/abs/1609.05158).
+    This layer requires a Convolution2D prior to it, having output filters computed according to
+    the formula :
+        filters = k * (scale_factor * scale_factor)
+        where k = a user defined number of filters (generally larger than 32)
+              scale_factor = the upscaling factor (generally 2)
+    This layer performs the depth to space operation on the convolution filters, and returns a
+    tensor with the size as defined below.
+    # Example :
+    ```python
+        # A standard subpixel upscaling block
+        x = Convolution2D(256, 3, 3, padding='same', activation='relu')(...)
+        u = SubPixelUpscaling(scale_factor=2)(x)
+        [Optional]
+        x = Convolution2D(256, 3, 3, padding='same', activation='relu')(u)
+    ```
+        In practice, it is useful to have a second convolution layer after the
+        SubPixelUpscaling layer to speed up the learning process.
+        However, if you are stacking multiple SubPixelUpscaling blocks, it may increase
+        the number of parameters greatly, so the Convolution layer after SubPixelUpscaling
+        layer can be removed.
+    # Arguments
+        scale_factor: Upscaling factor.
+        data_format: Can be None, 'channels_first' or 'channels_last'.
+    # Input shape
+        4D tensor with shape:
+        `(samples, k * (scale_factor * scale_factor) channels, rows, cols)` if data_format='channels_first'
+        or 4D tensor with shape:
+        `(samples, rows, cols, k * (scale_factor * scale_factor) channels)` if data_format='channels_last'.
+    # Output shape
+        4D tensor with shape:
+        `(samples, k channels, rows * scale_factor, cols * scale_factor))` if data_format='channels_first'
+        or 4D tensor with shape:
+        `(samples, rows * scale_factor, cols * scale_factor, k channels)` if data_format='channels_last'.
+    """
+
+    def __init__(self, scale_factor=2, data_format=None, **kwargs):
+        super(SubPixelUpscaling, self).__init__(**kwargs)
+
+        self.scale_factor = scale_factor
+        self.data_format = normalize_data_format(data_format)
+
+    def build(self, input_shape):
+        pass
+
+    def call(self, x, mask=None):
+        y = K_BACKEND.depth_to_space(x, self.scale_factor, self.data_format)
+        return y
+
+    def compute_output_shape(self, input_shape):
+        if self.data_format == 'channels_first':
+            b, k, r, c = input_shape
+            return (b, k // (self.scale_factor ** 2), r * self.scale_factor, c * self.scale_factor)
+        else:
+            b, r, c, k = input_shape
+            return (b, r * self.scale_factor, c * self.scale_factor, k // (self.scale_factor ** 2))
+
+    def get_config(self):
+        config = {'scale_factor': self.scale_factor,
+                  'data_format': self.data_format}
+        base_config = super(SubPixelUpscaling, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
